5fdcb3cea923f190a3dd3216407217762e4e0df7
freitagdavid <freitagdavid@outlook.com>
2019-02-04 17:57:46 -0500

    migrations/20190204154506_create_addresses_table: Removed some fields

--- migrations/20190204154506_create_addresses_table.js
+++ migrations/20190204154506_create_addresses_table.js
@@ -1,11 +1,7 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('addresses', tbl => {
         tbl.increments();
-        tbl.string('street_address').notNullable();
-        tbl.string('city').notNullable();
-        tbl.string('zip_code').notNullable();
-        tbl.string('country').notNullable();
-        tbl.string('building_number');
+        tbl.string('address').notNullable();
     });
 };
 
