32da8bb4aba902ee066d4bacd7daf6b6c682a864
freitagdavid <freitagdavid@outlook.com>
2019-02-04 18:47:03 -0500

    helpers/random: updates

--- helpers/random.js
+++ helpers/random.js
@@ -1,5 +1,13 @@
 const random = (a = 0, b = 10) => {
-    return Math.random() * (b - a) + a;
+    return Math.floor(Math.random() * (b - a) + a);
 };
 
-module.exports = random;
+const reallyRandom = (a = 0, b = 10) => {
+    if (random(0, 10) > 5) {
+        return random(1, 100);
+    } else {
+        return null;
+    }
+};
+
+module.exports = { random, reallyRandom };
