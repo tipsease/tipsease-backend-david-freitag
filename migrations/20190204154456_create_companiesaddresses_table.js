cf2cd413e21385242e7faefb9e645cf2e35f165c
freitagdavid <freitagdavid@outlook.com>
2019-02-04 15:48:54 -0500

    migrations/20190204154456_create_companiesaddresses_table: create

---
+++ migrations/20190204154456_create_companiesaddresses_table.js
@@ -0,0 +1,17 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('companiesaddresses', tbl => {
+        tbl.increments();
+        tbl.integer('company_id')
+            .unsigned()
+            .references('id')
+            .inTable('companies')
+            .notNullable();
+        tbl.integer('address_id')
+            .unsigned()
+            .references('id')
+            .inTable('addresses')
+            .notNullable();
+    });
+};
+
+exports.down = function(knex, Promise) {};
