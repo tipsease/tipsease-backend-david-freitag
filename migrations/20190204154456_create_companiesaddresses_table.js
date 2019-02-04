8ed32e315c632e794cba43a3b4000a64a867743d
freitagdavid <freitagdavid@outlook.com>
2019-02-04 15:49:51 -0500

    migrations/20190204154456_create_companiesaddresses_table: Slight update

--- migrations/20190204154456_create_companiesaddresses_table.js
+++ migrations/20190204154456_create_companiesaddresses_table.js
@@ -14,4 +14,6 @@ exports.up = function(knex, Promise) {
     });
 };
 
-exports.down = function(knex, Promise) {};
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('companiesaddresses');
+};
