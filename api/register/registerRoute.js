35985c07b62cd5503b457772072680c7def185ac
freitagdavid <freitagdavid@outlook.com>
2019-02-06 16:26:27 -0500

    api/register/registerRoute: Refactored register

--- api/register/registerRoute.js
+++ api/register/registerRoute.js
@@ -2,13 +2,12 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const { genToken } = require('../../middleware/auth');
 const imageParser = require('../../configs/cloudinary');
-const { tippers, tippees } = require('../../models');
+const { tippers, tippees, companies } = require('../../models');
 const QRCode = require('qrcode');
 const cloudinary = require('cloudinary');
 
 router.route('/').post(imageParser.single('image'), async (req, res) => {
-    const { tipperBoolean, ...data } = req.body;
-    console.log(data);
+    let { tipperBoolean, ...data } = req.body;
     const image = {};
     const hash = bcrypt.hashSync(data.password, 8);
 
@@ -28,51 +27,63 @@ router.route('/').post(imageParser.single('image'), async (req, res) => {
     }
 
     if (tipperBoolean) {
-        tippers
-            .insert(data)
-            .then(id => {
-                tippers.getById(id[0]).then(data => {
-                    const token = genToken(data);
-                    data[0].token = token;
-                    res.status(201).json(data);
-                });
-            })
-            .catch(res.status(500).json(err));
+        try {
+            const [tipperId] = await tippers.insert(data);
+            const tipper = await tippers.getById(tipperId);
+            const token = genToken(tipper);
+            tipper[0].token = token;
+            res.status(201).json(tipper);
+        } catch (err) {
+            res.status(500).json(err);
+        }
     } else {
-        if ((!data.start_date, !data.tagline, !data.company_id)) {
+        if ((!data.start_date, !data.tagline, !data.company_name)) {
             res.status(400).json({
-                errMessage: `first_name, last_name, email, password, start_date, tagline, and tipperBoolean are required`,
+                errMessage: `first_name, last_name, email, password, start_date, tagline, company_name and tipperBoolean are required`,
             });
             return;
         }
+        let qrCodeImg = {};
+        try {
+            qrCodeImg = await QRCode.toDataURL(
+                `We need to figure what to do with this`
+            );
+        } catch (err) {
+            console.log(err);
+        }
 
-        QRCode.toDataURL(`We need to figure out what to do with this`).then(
-            (url, err) => {
-                cloudinary.v2.uploader.upload(url, function(err, cloud) {
-                    data.qr_url = cloud.url;
-                    data.qr_id = cloud.public_id;
-                    tippees
-                        .insert(data)
-                        .then(id => {
-                            tippees.getById(id[0]).then(data => {
-                                const token = genToken(data);
-                                data[0].token = token;
-                                res.status(201).json(data);
-                            });
-                        })
-                        .catch(err => {
-                            if (err.errno === 19) {
-                                res.status(400).json({
-                                    errMessage: 'Emails must be unique',
-                                    errCode: 1,
-                                });
-                                return;
-                            }
-                            res.status(500).json(err);
-                        });
-                });
+        const genQr = async () => {
+            let count = 0;
+            let maxRetries = 10;
+            while (true) {
+                try {
+                    const cloud = await cloudinary.v2.uploader.upload(
+                        qrCodeImg
+                    );
+                    const output = {
+                        qr_url: cloud.url,
+                        qr_id: cloud.public_id,
+                    };
+                    return output;
+                } catch (err) {
+                    if (++count === maxRetries) throw err;
+                }
             }
-        );
+        };
+
+        const qrCode = await genQr(qrCodeImg);
+        data = { ...data, ...qrCode };
+
+        try {
+            const { company_name, ...inputData } = data;
+            const tippeeId = await tippees.insert(inputData);
+            const tippee = await tippees.getById(tippeeId[0]);
+            const token = genToken(tippee);
+            tippee[0].token = token;
+            res.status(201).json(tippee);
+        } catch (err) {
+            res.status(500).json(err);
+        }
     }
 });
 
