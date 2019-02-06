21a21281c8342b583fff8bd56d3094d6dd7d0a79
freitagdavid <freitagdavid@outlook.com>
2019-02-06 17:23:55 -0500

    Various stuff

--- models/index.js
+++ models/index.js
@@ -2,3 +2,4 @@ exports.tippers = require('./tippersModel');
 exports.tippees = require('./tippeesModel');
 exports.tips = require('./tipsModel');
 exports.companies = require('./companiesModel');
+exports.companiesTippees = require('./companiesTippeesModel');
